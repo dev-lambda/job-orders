fc4b28abd27490f3cbcc8b48f20b5057ec44f5ca
Draltan Marin <moi@draltan.com>
2023-06-05 11:49:16 +0200

    doc: fix edit link

--- packages/doc/docusaurus.config.js
+++ packages/doc/docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/dev-lambda/job-orders/edit/main/website/',
+            'https://github.com/dev-lambda/job-orders/edit/main/packages/doc/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
